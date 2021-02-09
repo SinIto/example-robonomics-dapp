91437b39410b9ee3b4bed378e7930cfb4412eebf
vol4tim <vol4tim@gmail.com>
2021-02-09 10:20:40 +0400

    connect to robonomics

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <template>
   <div id="app">
     <h1>Robonomics dApp</h1>
+    <div v-if="load">...</div>
+    <template v-else>
+      <div v-if="error" class="error">{{ error }}</div>
+      <template v-else-if="api && account">
+        <p>
+          Account: <b>{{ account }}</b> {{ balance }} |
+          <button @click="faucet">faucet</button>
+        </p>
+      </template>
+    </template>
   </div>
 </template>
 
 <script>
+import { initApi, initAccount, getBalance, faucet } from "./utils/api";
+import { formatBalance } from "@polkadot/util";
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      load: false,
+      api: null,
+      account: null,
+      balance: 0,
+      error: null
+    };
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      try {
+        this.load = true;
+        this.api = await initApi();
+        this.account = await initAccount();
+        getBalance(this.account, balance => {
+          this.balance = formatBalance(balance);
+        });
+        this.load = false;
+      } catch (error) {
+        this.error = error.message;
+        this.load = false;
+      }
+    },
+    faucet() {
+      faucet(this.account);
+    }
+  }
 };
 </script>
 
@@ -18,5 +61,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  font-size: 14px;
+  padding: 5px 12px;
+}
+button:hover {
+  cursor: pointer;
+}
+input {
+  font-size: 14px;
+  padding: 5px;
+}
+select {
+  font-size: 14px;
+  padding: 5px;
+}
+button:focus,
+input:focus {
+  outline: none;
+}
+.error {
+  color: rgb(151, 31, 31);
+  font-weight: bold;
+  text-align: center;
+  margin: 10px 0;
 }
 </style>
